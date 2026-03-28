a8e0e20099e7f9e5f354da07cfd84ca4021d727d
Eman Gurung <egurung1117@gmail.com>
2026-03-28 12:16:01 -0700

    front-end setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="homepage-container">
+            <h1>HOME</h1>
+        </div>
+    )
+}
+
+export default Home
