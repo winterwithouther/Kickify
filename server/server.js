d9595c30ab122d649e4123981e5382fe00a14f30
Eman Gurung <egurung1117@gmail.com>
2026-03-28 13:13:36 -0700

    created front end and back end and set up render configs

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Kickify API is running')
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+});
